7c7fc31ea1ed52591736f4b55bbe50db5ed84166
Russ Perry <rperry1886@gmail.com>
2020-08-12 23:40:38 -0400

    Add play pause functionality

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
+const video = getEl('video');
+const progressRange = getEl('.progress-range');
+const progressBar = getEl('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = getEl('.volume-range');
+const volumeBar = getEl('.volume-bar');
+const currentTime = getEl('.time-elapsed');
+const durationTime = getEl('.time-duration');
+const fullscreenBtn = getEl('.fullscreen');
 
+// Function to quickly assign element constants --- //
+function getEl(el) {
+  return document.querySelector(el);
+}
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon() {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+}
 
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
 
+// show play button when video ends
+video.addEventListener('ended', showPlayIcon);
 
-// Progress Bar ---------------------------------- //
+// Progress Bar ----------------------------------- //
 
+// Volume Controls -------------------------------- //
 
+// Change Playback Speed -------------------------- //
 
-// Volume Controls --------------------------- //
+// Fullscreen ------------------------------------- //
 
-
-
-// Change Playback Speed -------------------- //
-
-
-
-// Fullscreen ------------------------------- //
-
-
+// Event Listeners -------------------------------- //
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
