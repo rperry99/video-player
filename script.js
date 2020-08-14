6a3d2ae403c5b97151beb195d96d62af2195aad1
Russ Perry <rperry1886@gmail.com>
2020-08-13 21:57:06 -0400

    Add progress bar functionality

--- script.js
+++ script.js
@@ -36,6 +36,11 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ----------------------------------- //
 
+// Update Progress bar as video plays
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
+
 // Volume Controls -------------------------------- //
 
 // Change Playback Speed -------------------------- //
@@ -45,3 +50,5 @@ video.addEventListener('ended', showPlayIcon);
 // Event Listeners -------------------------------- //
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
