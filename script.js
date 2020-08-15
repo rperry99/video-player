1d78fc444bac97925870b35ab8514ddb02848e3d
Russ Perry <rperry1886@gmail.com>
2020-08-14 22:59:01 -0400

    Add volume functionality

--- script.js
+++ script.js
@@ -61,6 +61,29 @@ function setProgress(e) {
 
 // Volume Controls ------------------------------------------------------- //
 
+// volume bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // Rounding Volume up or down
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  // Change Icon based on volume
+  volumeIcon.className = ''; // Resets all classes
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down');
+  } else if (volume === 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-off');
+  }
+}
+
 // Change Playback Speed ------------------------------------------------- //
 
 // Fullscreen ------------------------------------------------------------ //
@@ -71,3 +94,4 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
