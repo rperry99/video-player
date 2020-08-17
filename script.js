1f64d827eb099a9820abeabbb20ecbe4bd6c3f51
Russ Perry <rperry1886@gmail.com>
2020-08-16 20:03:51 -0400

    Add fullscreen functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = getEl('.player');
 const video = getEl('video');
 const progressRange = getEl('.progress-range');
 const progressBar = getEl('.progress-bar');
@@ -117,6 +118,44 @@ function changeSpeed() {
 
 // Fullscreen ------------------------------------------------------------ //
 
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+  video.classList.add('video-fullscreen');
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+  video.classList.remove('video-fullscreen');
+}
+
+let fullscreen = false;
+
+// Toggle fullscreen
+function toggleFullscreen() {
+  if (!fullscreen) {
+    openFullscreen(player);
+  } else {
+    closeFullscreen();
+  }
+  fullscreen = !fullscreen;
+}
+
 // Event Listeners ------------------------------------------------------- //
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
@@ -126,3 +165,4 @@ progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
 speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
